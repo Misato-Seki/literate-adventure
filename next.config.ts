7f84a74e489acde445b2820cb9eb62e8db4bf4b9
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-02-26 09:06:01 +0200

    feat: add News feature

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* 他の設定オプション */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/media/file/**',
+      },
+    ],
+  },
+
 
   webpack: (config) => {
     // `child_process` と `fs` モジュールを無効化
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,10 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-slate-600 text-white">
       This is Home page.
+      <Link href="/news"><h1>NEWS</h1></Link>
     </div>
   );
 }
